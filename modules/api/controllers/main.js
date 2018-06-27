aa853a3be6e43a433a4dae46106a9162b8131c31
Shahrukh <ShahrukhMushtaq23@gmail.com>
2018-06-28 02:38:02 +0500

    i. in index.js change host value to 127.0.0.1 ii. in module.js define route to get single user from DB. iii. in main.js all function of api implemented. iv. creation of each user's token implementd

--- modules/api/controllers/main.js
+++ modules/api/controllers/main.js
@@ -1,10 +1,12 @@
 
-const User = require('../../../models/user');
+var User = require('../../../models/user');
 var sequelize = require('sequelize');
 var constants = require("../../../config/constants");
 var requestHelper = require("../../../helpers/request");
 var responseHelper = require("../../../helpers/response");
 var db = require('../../../index');
+var bcrypt = require('bcryptjs');
+var jwt = require('jsonwebtoken');
 var main = {
     title: "Hello World",
     statusCode: constants.HTTP.CODES.SUCCESS
@@ -14,105 +16,77 @@ function validateSignup(body) {
     if (body.name != null && body.email != null && body.password != null && body.age != null && body.phone != null && body.gender != null) {
         return true;
     }   ///*** Add Email regex here as well */
-
     return false;
 }
 
+main.me = function(req, res) {
+    var token = req.headers['x-access-token'];
+    if (!token) return res.status(401).send({ auth: false, message: 'No token provided.' });    
+    jwt.verify(token, "mySecret", function(err, decoded) {
+    if (err) return res.status(500).send({ auth: false, message: 'Failed to authenticate token.' });
+    console.log(decoded.id);
+     var sql = "SELECT * FROM users WHERE id=?";
+     let userId = [decoded.id];
+     db.db.query(sql , [userId],(err , user) => {
+         if (err) throw err;
+         res.status(200).send(user);
+         //console.log("User is",user);
+     });
+    });
+  }
+
 main.home = function (req, res, next) {
-
-    // console.log(db);
-    
+    // console.log(db);   
     db.db.query('SELECT * FROM users', null, (err, result)=> {
-        if(err) console.log(err);
-        else res.send(result);
-    });    
-   
+        if (err) throw err;
+        res.send(JSON.stringify({"status": 200, "response": result}));
+    });       
 }
-
-
-main.login = function (req, res, next) {
-    res.send('Success');
-}
-
-// exports.register = function (req, res) {
-//     // console.log("req",req.body);
-//     var users = {
-//         "id": req.body.id,
-//         "lname": req.body.name,
-//         "email": req.body.email,
-//         "password": req.body.password,
-//         "created": today,
-//         "modified": today
-//     }
-//     db.db.query('INSERT INTO users SET ?', users, function (error, results, fields) {
-//         if (error) {
-//             console.log("error ocurred", error);
-//             res.send({
-//                 "code": 400,
-//                 "failed": "error ocurred"
-//             })
-//         } else {
-//             console.log('The solution is: ', results);
-//             res.send({
-//                 "code": 200,
-//                 "success": "user registered sucessfully"
-//             });
-//         }
-//     });
-// }
 
 main.signup = function (req, res, next) {
-    //var postBody = requestHelper.parseBody(req.body);
-    //var postBody = req.body;
-    //var responseBody = {};
-    // let newUser ={        
-    //     name: req.body.name,
-    //     email: req.body.email,
-    //     password: req.body.password,
-    //     age: req.body.age,
-    //     phone: req.body.phone,
-    //     gender: req.body.gender
-    // };
-    //const user = req.body;
+   // const user = req.body;
     //console.log(user);
+    var hash = bcrypt.hashSync(req.body.password, 8);
+
     var sql ="INSERT INTO users (name,email,password,age,phone,gender) VALUES ?";
     let userr = [
-        [req.body.name, req.body.email, req.body.password, req.body.age, req.body.phone, req.body.gender]
+        [req.body.name, req.body.email, hash, req.body.age, req.body.phone, req.body.gender]
     ];
-    db.db.query(sql, [userr], function (err, result) {
+    db.db.query(sql, [userr], function (err, user) {
     if (err) throw err;
-    console.log("1 record inserted", result);
-
+    console.log(user);
+    // create a token
+    var token = jwt.sign({ id: user.insertId }, "mySecret", {
+        expiresIn: 86400 // expires in 24 hours
+      });
+    console.log("1 record inserted", user.insertId);
+    res.status(200).send({ auth: true, token: token });
   });
-    //  if(validateSignup(postBody)){
-    //       User.build(postBody).save().then(function(response){
-    //           responseBody = responseHelper.formatResponse(
-    //                                                   constants.MESSAGES.SIGNUP.SUCCESS,
-    //                                                   {id:response.id,name:response.name}
-    //                                       )
-    //           res.statusCode = constants.HTTP.CODES.SUCCESS;
-    //           res.send(responseBody);
-    //       }).catch(function(error){
+}
 
-    //           responseBody = responseHelper.formatResponse(
-    //                                       constants.MESSAGES.SIGNUP.ERROR,
-    //                                       {}
-    //                                       )
-    //           res.statusCode = constants.HTTP.CODES.SUCCESS;
-    //           res.send(responseBody);
-    //       });
+main.login = function (req, res, next) {
 
-    //  }else{
+    var sql_query = "SELECT * FROM users WHERE email=?";
+    let emId = [req.body.email];
+    db.db.query(sql_query ,[emId] , (err , result)=> {
+        if (err) return res.status(500).send('Error on the server.');
+        if (!result) return res.status(404).send('No user found.');
+        console.log(req.body.password);
+        console.log(result[0].password);
+        //result.toString();
+        var passwordIsValid = bcrypt.compareSync(req.body.password, result[0].password);
+        console.log(passwordIsValid);
+        console.log(result[0].id);
+        if (!passwordIsValid) return res.status(401).send({ auth: false, token: null });
+        var token = jwt.sign({ id : result[0].id},"mySecret",{
+            expiresIn: 86400
+        });
+        res.status(200).send({ auth: true, token: token });
+        
+    });
+  }
 
-    //           responseBody = responseHelper.formatResponse(
-    //                                       constants.MESSAGES.GENERAL.FIELDS_REQUIRED,
-    //                                       {}
-    //                                       )
-
-    //           res.statusCode = constants.HTTP.CODES.BAD_REQUEST;
-    //           res.send(responseBody);
-    //  }  
+main.logout = function(req , res ){
+    res.status(200).send({ auth: false , token: null});
 }
 module.exports = main;
-
-
--- modules/api/module.js
+++ modules/api/module.js
@@ -3,10 +3,11 @@ var router = express.Router();
 var main = require('./controllers/main');
 
 /* Registering All the routes */
-router.get('/', main.home);
+router.get('/me', main.me);  
+router.get('/users', main.home);
 router.post('/login', main.login);
 router.post('/signup', main.signup);
-
+router.get('/logout', main.logout);
 /****/
 
 
