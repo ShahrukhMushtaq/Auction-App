c49b875e82fc027a030a28bbf56bba04f2adc1a3
Shahrukh <ShahrukhMushtaq23@gmail.com>
2018-06-29 01:32:39 +0500

    started work on front-end, integrate angular-material in application.

--- auction-front/src/app/app.module.ts
+++ auction-front/src/app/app.module.ts
@@ -1,4 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {NoopAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonModule, MatCheckboxModule} from '@angular/material';
+
 import { NgModule } from '@angular/core';
 
 
@@ -10,7 +14,10 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCheckboxModule
   ],
   providers: [],
   bootstrap: [AppComponent]
